f4874ccaf8ceb071612b368e0d2ee9b2b8e3c794
greenlemonT <ms950219@naver.com>
2024-01-24 11:53:47 +0900

    init

---
+++ src/main/java/GDSC/gamzamap/GamzamapApplication.java
@@ -0,0 +1,13 @@
+package GDSC.gamzamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamzamapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamzamapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDSC/gamzamap/GamzamapApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSC.gamzamap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamzamapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
