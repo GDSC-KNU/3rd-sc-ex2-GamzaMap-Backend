29142841ecf5aa6bf797a1fbd9de70a3f5e2d928
izoq78 <pyjung1209@naver.com>
2024-02-06 02:31:18 +0900

    [Feat]: 찜 기능 구현

--- src/main/java/GDSC/gamzamap/Repository/MemberRepository.java
+++ src/main/java/GDSC/gamzamap/Repository/MemberRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByRefreshToken(String refreshToken);
+    Optional<Member> findById(Long id);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
 }
--- src/main/java/GDSC/gamzamap/Repository/StoreRepository.java
+++ src/main/java/GDSC/gamzamap/Repository/StoreRepository.java
@@ -1,7 +1,11 @@
 package GDSC.gamzamap.Repository;
 
+import GDSC.gamzamap.Entity.Member;
 import GDSC.gamzamap.Entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Optional<Store> findById(Long id);
 }
