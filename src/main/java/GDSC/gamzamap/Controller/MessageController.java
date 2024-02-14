464a265cfa21f21ee9234f8062833aa784091e9c
greenlemonT <ms950219@naver.com>
2024-02-15 00:06:37 +0900

    [Chore]: swagger 접근권한변경

--- src/main/java/GDSC/gamzamap/Controller/MessageController.java
+++ src/main/java/GDSC/gamzamap/Controller/MessageController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "찜 관련 API")
+@Tag(name = "메시지 관련 API")
 public class MessageController {
 
     private final MemberRepository memberRepository;
