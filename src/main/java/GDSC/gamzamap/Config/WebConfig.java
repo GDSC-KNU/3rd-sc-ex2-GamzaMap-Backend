e85e4598bcdfec6e785d7b7b1c31050b3c9fc44d
greenlemonT <ms950219@naver.com>
2024-02-21 22:00:58 +0900

    [Fix]: cors error 3rd trial

--- src/main/java/GDSC/gamzamap/Config/WebConfig.java
+++ src/main/java/GDSC/gamzamap/Config/WebConfig.java
@@ -1,19 +1,33 @@
 package GDSC.gamzamap.Config;
 
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(6000);
-    }
+   @Bean
+   public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean(){
+       CorsConfiguration config=new CorsConfiguration();
+       config.setAllowCredentials(false);
+       config.addAllowedOrigin("*");
+       config.addAllowedHeader("*");
+       config.addAllowedMethod("*");
+       config.setMaxAge(6000L);
+
+       UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+       source.registerCorsConfiguration("/**",config);
+
+       FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new
+               CorsFilter());
+       filterBean.setOrder(0);
+
+       return filterBean;
+   }
+
 }
