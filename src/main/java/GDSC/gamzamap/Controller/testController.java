c94212b60adc36044e71a3c1f59af21a4ce9cd32
greenlemonT <ms950219@naver.com>
2024-02-21 21:26:31 +0900

    [Fix]: cors error 2nd trial

--- src/main/java/GDSC/gamzamap/Controller/testController.java
+++ src/main/java/GDSC/gamzamap/Controller/testController.java
@@ -3,12 +3,14 @@ package GDSC.gamzamap.Controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://192.168.0.13:5173")
 public class testController {
     @GetMapping("/test")
     public ResponseEntity<String> test(){
